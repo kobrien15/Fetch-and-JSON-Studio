c8bfd9cf467bacd2b6296bdd9ce25a45bc2cb0d8
Kyle O'Brien <kobrien15@gmail.com>
2019-12-15 09:09:35 -0600

    Completed studio, except sort Bonus Mission

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
-// TODO: add code here
+window.addEventListener("load", function(){
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+        response.json().then(function(json){
+            const container = document.getElementById("container");
+            let astronauts = "";
+            let astronaut = "";
+            // json.sort(sortByHours(hoursInSpace));
+            for(let i = 0; i < json.length; i++) {
+                astronaut = `
+                    <div class="astronaut">
+                        <div class="bio">
+                        <h3>${json[i].firstName} ${json[i].lastName}</h3>
+                        <ul>
+                            <li>Hours in space: ${json[i].hoursInSpace}</li>
+                            <li id="active" style="">Active: ${json[i].active}</li>
+                            <li>Skills: ${json[i].skills.join(', ')}</li>
+                        </ul>
+                        </div>
+                        <img class="avatar" src="${json[i].picture}">
+                    </div>
+                `;
+                astronauts += astronaut;
+            }
+            container.innerHTML += astronauts;
+            
+            let active = document.querySelectorAll("#active");
+            for(let i=0; i < active.length; i++) {
+                if(active[i].innerText === "Active: true"){
+                    active[i].style.color = "green";
+                }
+            }
+
+            document.querySelector("#heading").innerHTML = `Astronauts (${json.length})`
+            
+        });
+    });
+
+});
+
